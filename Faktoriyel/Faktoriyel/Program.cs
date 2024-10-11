20d37ea02a04a6fd7ea1d4e7a713b349a643f8c8
Erkan <108893420+EfeErkanKostekci@users.noreply.github.com>
2024-10-11 15:25:43 +0300

    Add files via upload

---
+++ Faktoriyel/Faktoriyel/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        // Number entry from user
+        Console.Write("Faktöriyelini hesaplamak istediğiniz sayıyı girin: ");
+        string giris = Console.ReadLine();
+
+        // string to int conversion
+        int sayi;
+        if (int.TryParse(giris, out sayi) && sayi >= 0) // Negative number control
+        {
+            long faktoriyel = 1;
+
+            // Factorial calculation
+            for (int i = 1; i <= sayi; i++)
+            {
+                faktoriyel *= i;
+            }
+
+            // Print the result
+            Console.WriteLine($"{sayi}! = {faktoriyel}");
+        }
+        else
+        {
+            Console.WriteLine("Geçerli bir pozitif sayı girin.");
+        }
+    }
+}
